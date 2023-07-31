b4144ba2f933f3ff74a2735bdf68cfe6d871442c
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-07-31 19:15:06 +0530

    listed the todos from the input and focused the input

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import './Todo.css'
+import { useState, useRef, useEffect } from 'react';
+
 
 function Todo() {
+    const [todo, setTodo] = useState('')
+    const [todos, setTodos] = useState([])
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const addTodo = () => {
+        setTodos([...todos, todo])
+        setTodo('')
+    }
+
+    const inputRef = useRef('null')
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
     return (
         <div className='container'>
             <h2>TODO APP</h2>
-            <form className='form-group'>
-                <input type="text" placeholder='Enter Your Todo' className='form-control' />
-                <button>ADD</button>
+            <form className='form-group' onSubmit={handleSubmit}>
+                <input type="text" value={todo} ref={inputRef} placeholder='Enter Your Todo' className='form-control' onChange={(event) => setTodo(event.target.value)} />
+                <button onClick={addTodo}>ADD</button>
             </form>
             <div className='list'>
                 <ul>
-                    <li className='form-control'>First</li>
-                    <li className='form-control'>Second</li>
-                    <li className='form-control'>Third</li>
+                    {
+                        todos.map((to) => (
+                            <li>
+                                {to}
+                            </li>
+                        ))}
                 </ul>
             </div>
         </div>
